3ff61572f88f70c5c5ea738aaaa22d88cbe0fc47
AnastasiaMazaeva <blackdeadnegro@gmail.com>
2021-08-11 20:37:19 +0500

    [HW-10]: bytecodes (#5)
    Co-authored-by: mazaeva.av <mazaeva.av@esphere.ru>

---
+++ hw10-bytecodes/src/main/java/proxy/Proxy.java
@@ -0,0 +1,9 @@
+package proxy;
+
+public class Proxy {
+
+    public static void main(String[] args) {
+        TestLogging testLogging = new TestLogging();
+        testLogging.calculation(19, 31, "bla");
+    }
+}
---
+++ hw10-bytecodes/src/main/java/proxy/TestLogging.java
@@ -0,0 +1,23 @@
+package proxy;
+
+
+public class TestLogging {
+
+    @Log
+    public void calculation(int x) {
+        System.out.println(x);
+    }
+
+    @Log
+    public void calculation(int x, int y) {
+        int sum = x + y;
+        System.out.println(sum);
+    }
+
+    @Log
+    public void calculation(int x, int y, String z) {
+        String sum = z + x + y;
+        System.out.println(sum);
+    }
+
+}
