65be66bc31caa09bca25bdf45abdbc016ab3759f
mazaeva.av <mazaeva.av@esphere.ru>
2021-09-01 21:24:40 +0500

    [HW-15]: patterns

---
+++ hw15-patterns/src/main/java/ru/otus/exception/ApocalypseException.java
@@ -0,0 +1,8 @@
+package ru.otus.exception;
+
+public class ApocalypseException extends RuntimeException {
+
+    public ApocalypseException() {
+        super("Oh no! Even second, my only weakness!");
+    }
+}
