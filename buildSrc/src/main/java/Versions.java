cc0e3281e0df98c4786269e4682def89ab7c3032
mazaeva.av <mazaeva.av@esphere.ru>
2021-09-24 21:01:00 +0500

    [HW-01]: gradle

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,5 +1,6 @@
 public interface Versions {
 
+    String errorprone = "2.9.0";
     String guava = "30.1.1-jre";
     String log4j = "1.7.29";
     String asm = "9.1";
@@ -7,4 +8,8 @@ public interface Versions {
     String protobuf = "3.12.1";
     String jackson = "2.11.1";
     String javax = "1.1.4";
+
+    String flyway = "6.4.4";
+    String postgresql = "42.2.5";
+    String testcontainers = "1.15.3";
 }
