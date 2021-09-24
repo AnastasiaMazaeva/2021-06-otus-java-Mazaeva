5bcb6d06671b5642ebbbe8f0b660e820b4c6a983
AnastasiaMazaeva <blackdeadnegro@gmail.com>
2021-09-24 20:23:10 +0500

    [HW-16]: serializing (#8)
    * [HW-16]: serializing
    
    * [HW-16]: serializing
    
    Co-authored-by: mazaeva.av <mazaeva.av@esphere.ru>

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -3,4 +3,8 @@ public interface Versions {
     String guava = "30.1.1-jre";
     String log4j = "1.7.29";
     String asm = "9.1";
+    String glassfishJson = "1.1.4";
+    String protobuf = "3.12.1";
+    String jackson = "2.11.1";
+    String javax = "1.1.4";
 }
