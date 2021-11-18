ee1d7d3762350919adcd395fddf21b28af85a5b8
mazaeva.av <mazaeva.av@esphere.ru>
2021-11-18 12:30:09 +0500

    [HW-31]: Executors: executorService, именования потоков

--- hw31-executors/src/main/java/homeworks/executors/ExecutorsHome.java
+++ hw31-executors/src/main/java/homeworks/executors/ExecutorsHome.java
@@ -8,23 +8,8 @@ public class ExecutorsHome {
 
     public static void main(String[] args) {
         ScheduledExecutorService service = Executors.newScheduledThreadPool(2);
-        Runnable runnable = () -> {
-            try {
-                for (int i = 1; i <= 10; ++i) {
-                    System.out.println(Thread.currentThread().getName() + " : " + i + " ");
-                    Thread.sleep(1_000);
-                }
-                for (int i = 9; i >= 1; --i) {
-                    System.out.println(Thread.currentThread().getName() + " : " + i + " ");
-                    Thread.sleep(1_000);
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        };
-
-        service.schedule(runnable, 0, TimeUnit.SECONDS);
-        service.schedule(runnable, 1, TimeUnit.SECONDS);
+        service.schedule(new CustomThread("T1"), 0, TimeUnit.SECONDS);
+        service.schedule(new CustomThread("T2"), 1, TimeUnit.SECONDS);
         service.shutdown();
     }
 }
