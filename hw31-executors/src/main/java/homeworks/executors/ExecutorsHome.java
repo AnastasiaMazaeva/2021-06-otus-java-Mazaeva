f6e5e05a4cd9cadf3cacc0c3d062f58b6d2a6ba3
mazaeva.av <mazaeva.av@esphere.ru>
2021-11-18 22:36:31 +0500

    [HW-31]: Executors: singlethreadpool

--- hw31-executors/src/main/java/homeworks/executors/ExecutorsHome.java
+++ hw31-executors/src/main/java/homeworks/executors/ExecutorsHome.java
@@ -1,15 +1,41 @@
 package homeworks.executors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Future;
 
 public class ExecutorsHome {
+    private static final Logger log = LoggerFactory.getLogger(ExecutorsHome.class);
+    private static final ExecutorService service = Executors.newSingleThreadExecutor();
 
-    public static void main(String[] args) {
-        ScheduledExecutorService service = Executors.newScheduledThreadPool(2);
-        service.schedule(new CustomThread("T1"), 0, TimeUnit.SECONDS);
-        service.schedule(new CustomThread("T2"), 1, TimeUnit.SECONDS);
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        for (int i = 0; i <= 10; ++i) {
+            executeTasks(i);
+        }
+
+        for (int i = 9; i >= 1; --i) {
+            executeTasks(i);
+        }
         service.shutdown();
+    }
+
+    private static void executeTasks(int counter) throws InterruptedException, ExecutionException {
+        Future<Integer> futureFirst = service.submit(() -> task(counter));
+        Future<Integer> futureSecond = service.submit(() -> task(counter));
+        log.info("T1 : " + futureFirst.get());
+        log.info("T2 : " + futureSecond.get());
+    }
+
+    private static int task(int id) {
+        try {
+            Thread.sleep(1_000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        return id;
     }
 }
